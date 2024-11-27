1202c8ae78c34825ab1db09fc63e7f14f769a0b8
Kauan_kelvin <126915091+Kauankelv@users.noreply.github.com>
2024-11-27 02:11:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// Strings
+
+//const firstName = "Kauãn";
+//const lastName = "Kelvin";
+
+//const names = "Felipe, João, Julia";
+
+//console.log("Meu nome é " + firstName + " " + lastName);
+//console.log(`Meu nome é ${firstName.toUpperCase()} ${lastName.toLowerCase()}`);
+
+//console.log(names.split(","));
+
+//Numbers
+//const number = 5;
+
+//console.log(number.toString());
+//console.log(typeof number.toString());
+
+//Booleanos
+//false
+//true
+
+//console.log(2 == 2)
+
+// Null & Undefined
+
+//const x = null;
+
+//const y = undefined;
