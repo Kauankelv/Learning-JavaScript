0ae0a1302aff486ef965f60e949c3a9a089e322e
Kauan_kelvin <126915091+Kauankelv@users.noreply.github.com>
2024-11-27 16:04:08 -0300

    Update and rename main.js to 3-Aula-Funções-de-Lista.js

---
+++ 3-Aula-Funções-de-Lista.js
@@ -0,0 +1,20 @@
+const numbers = [1, 2, 3, 4, 5];
+
+//função map faz alteração na lista acima
+const numbersMultipliedByTwo = numbers.map(function (number) {
+  return number * 2;
+});
+
+const ages = [8, 13, 27, 30, 22, 40];
+
+//filtra a lista de acordo com uma função esta esta para ver se o numero da lista acima e par
+const evenAges = ages.filter(function (age) {
+  return age % 2 === 0;
+});
+
+//função de soma de lista
+const sumOFAges = ages.reduce(function (age, accumulator) {
+  return accumulator + age;
+}, 0);
+
+console.log(sumOFAges);
